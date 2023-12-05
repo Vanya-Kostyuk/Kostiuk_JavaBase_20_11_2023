3e2115f8be792f37f8a0e226b775aca6b85c622a
Kostiuk Ivan <ivantayoy@gmail.com>
2023-12-05 14:33:49 +0200

    HW6

---
+++ src/main/java/ua/hillel/kostiuk/lessons/lesson5/HW6/CounterStrike.java
@@ -0,0 +1,79 @@
+package ua.hillel.kostiuk.lessons.lesson5.HW6;
+
+import java.util.Scanner;
+
+public class CounterStrike {
+    public static void main(String[] args) {
+//        team 1
+        String nameTeam1;
+        int fragsFistPlayerTeam1 = 0;
+        int fragsSecondPlayerTeam1 = 0;
+        int fragsThirdPlayerTeam1 = 0;
+        int fragsPlayerForthTeam1 = 0;
+        int fragsPlayerFifthTeam1 = 0;
+
+//        team 2
+        String nameTeam2;
+        int fragsFistPlayerTeam2 = 0;
+        int fragsSecondPlayerTeam2 = 0;
+        int fragsThirdPlayerTeam2 = 0;
+        int fragsPlayerForthTeam2 = 0;
+        int fragsPlayerFifthTeam2 = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введіть ім'я першої команди : ");
+        nameTeam1 = scanner.nextLine();
+        System.out.println("Team 1 : " + nameTeam1);
+        System.out.println("Введіть кількість фрагів для 5-ти гравців першої команди : ");
+        if(scanner.hasNextInt()) {
+            fragsFistPlayerTeam1 = scanner.nextInt();
+            fragsSecondPlayerTeam1 = scanner.nextInt();
+            fragsThirdPlayerTeam1 = scanner.nextInt();
+            fragsPlayerForthTeam1 = scanner.nextInt();
+            fragsPlayerFifthTeam1 = scanner.nextInt();
+        }  else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        scanner.nextLine();
+
+        System.out.println("Введіть ім'я другої команди : ");
+        nameTeam2 = scanner.nextLine();
+        System.out.println("Team 2 : " + nameTeam2);
+        System.out.println("Введіть кількість фрагів для 5-ти гравців другої команди : ");
+        if(scanner.hasNextInt()) {
+            fragsFistPlayerTeam2 = scanner.nextInt();
+            fragsSecondPlayerTeam2 = scanner.nextInt();
+            fragsThirdPlayerTeam2 = scanner.nextInt();
+            fragsPlayerForthTeam2 = scanner.nextInt();
+            fragsPlayerFifthTeam2 = scanner.nextInt();
+        }  else {
+            System.out.println("Wrong data! Restart program!!!");
+            System.exit(0);
+        }
+
+        int countPlayers = 5;
+        int resultTeam1 = fragsFistPlayerTeam1 + fragsSecondPlayerTeam1 + fragsThirdPlayerTeam1
+                + fragsPlayerForthTeam1 + fragsPlayerFifthTeam1;
+
+        int avgTeam1 = ((fragsFistPlayerTeam1 + fragsSecondPlayerTeam1 + fragsThirdPlayerTeam1
+                + fragsPlayerForthTeam1 + fragsPlayerFifthTeam1) / countPlayers);
+        System.out.println("Середнє арифметичне team " + nameTeam1 + " : " + avgTeam1);
+
+        int resultTeam2 = fragsFistPlayerTeam2 + fragsSecondPlayerTeam2 + fragsThirdPlayerTeam2
+                + fragsPlayerForthTeam2 + fragsPlayerFifthTeam2;
+
+        int avgTeam2 = ((fragsFistPlayerTeam2 + fragsSecondPlayerTeam2 + fragsThirdPlayerTeam2
+                + fragsPlayerForthTeam2 + fragsPlayerFifthTeam2) / countPlayers);
+        System.out.println("Середнє арифметичне team " + nameTeam2 + " : " + avgTeam2);
+
+        if(resultTeam1 > resultTeam2) {
+            System.out.println("Перемогла команда " + nameTeam1 + " набрала " + resultTeam1 + " очків");
+        } else if (resultTeam1 < resultTeam2) {
+            System.out.println("Перемогла команда " + nameTeam2 + " набрала " + resultTeam2 + " очків ");
+        } else {
+            System.out.println("Нічия!");
+        }
+    }
+}
