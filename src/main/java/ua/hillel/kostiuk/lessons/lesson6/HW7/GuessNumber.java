018cd117866d423f68629c6a52328c1b7116e48e
Kostiuk Ivan <ivantayoy@gmail.com>
2023-12-15 13:35:46 +0200

    HW7

---
+++ src/main/java/ua/hillel/kostiuk/lessons/lesson6/HW7/GuessNumber.java
@@ -0,0 +1,40 @@
+package ua.hillel.kostiuk.lessons.lesson6.HW7;
+
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() * 11);
+        System.out.println("- The number is guessed-");
+        Scanner scanner = new Scanner(System.in);
+        int yourNumber;
+        int min = 0;
+        int max = 10;
+        int attempts = 5;
+
+        while (attempts > 0) {
+            System.out.println("Enter your number from 0 to 10: ");
+            if(scanner.hasNextInt()) {
+                yourNumber = scanner.nextInt();
+                if(yourNumber == randomNumber) {
+                    System.out.println("You are win");
+                    break;
+                } else if (yourNumber < min || yourNumber > max) {
+                    System.out.println("From 0 to 10 !!");
+                } else {
+                    attempts--;
+                    if(attempts > 0) {
+                        System.out.println("Try again. Attempts - " + attempts);
+                    } else {
+                        System.out.println("You are lose. The number: " + randomNumber);
+                    }
+                }
+            } else {
+                System.out.println("Wrong data. Enter a number!");
+                if(scanner.hasNextInt()) {
+                    scanner.nextInt();
+                }
+            } scanner.nextLine();
+        }
+    }
+}
