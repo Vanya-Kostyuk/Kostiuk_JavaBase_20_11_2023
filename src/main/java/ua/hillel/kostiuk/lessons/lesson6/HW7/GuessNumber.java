572df5dd2545a0485ecb5a39617a8d6c6fd0da0a
Kostiuk Ivan <ivantayoy@gmail.com>
2023-12-15 22:33:07 +0200

    HW7 refactor

--- src/main/java/ua/hillel/kostiuk/lessons/lesson6/HW7/GuessNumber.java
+++ src/main/java/ua/hillel/kostiuk/lessons/lesson6/HW7/GuessNumber.java
@@ -7,23 +7,23 @@ public class GuessNumber {
         int randomNumber = (int) (Math.random() * 11);
         System.out.println("- The number is guessed-");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number from 0 to 10:");
         int yourNumber;
         int min = 0;
         int max = 10;
         int attempts = 5;
 
         while (attempts > 0) {
-            System.out.println("Enter your number from 0 to 10: ");
-            if(scanner.hasNextInt()) {
+            if (scanner.hasNextInt()) {
                 yourNumber = scanner.nextInt();
-                if(yourNumber == randomNumber) {
+                if (yourNumber == randomNumber) {
                     System.out.println("You are win");
                     break;
                 } else if (yourNumber < min || yourNumber > max) {
                     System.out.println("From 0 to 10 !!");
                 } else {
                     attempts--;
-                    if(attempts > 0) {
+                    if (attempts > 0) {
                         System.out.println("Try again. Attempts - " + attempts);
                     } else {
                         System.out.println("You are lose. The number: " + randomNumber);
@@ -31,10 +31,8 @@ public class GuessNumber {
                 }
             } else {
                 System.out.println("Wrong data. Enter a number!");
-                if(scanner.hasNextInt()) {
-                    scanner.nextInt();
-                }
-            } scanner.nextLine();
+            }
+            scanner.nextLine();
         }
     }
 }
